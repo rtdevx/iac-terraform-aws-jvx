4ed13d9799fec1c794d066a0312cb88a1cd8411b
robk <robk@localhost>
2026-01-27 09:41:15 +0000

    [HL-46] updated app-install User Data sequence

--- terraform-manifests/app1-install.sh
+++ terraform-manifests/app1-install.sh
@@ -4,7 +4,7 @@ set -e
 yum update -y
 yum install -y java-17-amazon-corretto-headless awscli
 
-APP_VERSION=$(aws ssm get-parameter --name "/java-app1/version" --query "Parameter.Value" --output text)
-aws s3 cp s3://my-app-artifacts/java-app1-$APP_VERSION.jar /opt/java-app1.jar
+APP_VERSION=$(aws ssm get-parameter --name "/jvx/version" --query "Parameter.Value" --output text)
+aws s3 cp s3://rk-artifact/jvx-$APP_VERSION.jar /opt/jvx.jar
 
-nohup java -jar /opt/java-app1.jar > /var/log/java-app1.log 2>&1 &
+nohup java -jar /opt/jvx.jar > /var/log/jvx.log 2>&1 &
