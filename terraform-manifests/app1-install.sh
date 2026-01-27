cda53ce9a75176d81af2b90e1c3d5f5a579f32e7
robk <robk@localhost>
2026-01-27 17:51:28 +0000

    [HL-46] Modifying app1-install.sh to install Java

--- terraform-manifests/app1-install.sh
+++ terraform-manifests/app1-install.sh
@@ -7,4 +7,4 @@ yum install -y java-17-amazon-corretto-headless awscli
 APP_VERSION=$(aws ssm get-parameter --name "/jvx/version" --query "Parameter.Value" --output text)
 aws s3 cp s3://rk-artifact/jvx-$APP_VERSION.jar /opt/jvx.jar
 
-nohup java -jar /opt/jvx.jar > /var/log/jvx.log 2>&1 &
+nohup java -jar /opt/jvx.jar --server.port=80 > /var/log/jvx.log 2>&1 &
