631361772db800119bd7bb3b6071d267afdc2aab
robk <robk@localhost>
2026-05-19 11:05:51 +0100

    [HL-66] Updated Security Groups for RDS

--- terraform-manifests/c5-06-securitygroup-rdsdbsg.tf
+++ terraform-manifests/c5-06-securitygroup-rdsdbsg.tf
@@ -14,7 +14,8 @@ resource "aws_security_group" "private-db-3306" {
 resource "aws_vpc_security_group_ingress_rule" "private-db-3306_ipv4" {
   description       = "Allow Port 3306 INBOUND"
   security_group_id = aws_security_group.private-db-3306.id
-  cidr_ipv4         = var.vpc_cidr
+  //cidr_ipv4         = var.vpc_cidr
+  referenced_security_group_id = aws_security_group.private-web-8080.id
   from_port         = 3306
   ip_protocol       = "tcp"
   to_port           = 3306
@@ -27,7 +28,7 @@ resource "aws_vpc_security_group_ingress_rule" "private-db-3306_ipv4" {
 # INFO: Create Egress Security Group - ALL
 
 resource "aws_security_group" "db-egress" {
-  name        = "db-egress"
+  name = "${local.name}-private-db-egress"
   description = "${local.name}-db-egress"
   vpc_id      = module.vpc.vpc_id
 
@@ -37,7 +38,7 @@ resource "aws_security_group" "db-egress" {
 resource "aws_vpc_security_group_egress_rule" "db-allow-all-traffic_ipv4" {
   description       = "Allow all IP and ports OUTBOUND"
   security_group_id = aws_security_group.db-egress.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = var.vpc_cidr # NOTE: Outbound only to VPC CIDR required for AWS Services (KMS, CloudWatch Logs, Secrets Manager). Communication via NAT or VPC Endpoints (if present)
   ip_protocol       = "-1" # semantically equivalent to all ports
 
   tags = local.common_tags
